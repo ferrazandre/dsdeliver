24824f69b07a26ac05588c76ec4b1ab2009cb0fb
ferrazandre <55859517+ferrazandre@users.noreply.github.com>
2021-08-01 17:26:59 -0300

    POST/ orders

--- backend/src/main/java/com/dev/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/dev/dsdeliver/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.dev.dsdeliver.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,8 +9,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.dev.dsdeliver.dto.OrderDTO;
+import com.dev.dsdeliver.dto.ProductDTO;
 import com.dev.dsdeliver.model.Order;
+import com.dev.dsdeliver.model.OrderStatus;
+import com.dev.dsdeliver.model.Product;
 import com.dev.dsdeliver.repositories.OrderRepository;
+import com.dev.dsdeliver.repositories.ProductRepository;
 
 @Service
 public class OrderService {
@@ -17,15 +22,31 @@ public class OrderService {
 	@Autowired
 	private OrderRepository orderRepository;
 
+	@Autowired
+	private ProductRepository productRepository;
+
 	@Transactional(readOnly = true)
 	public List<OrderDTO> findAll() {
 		List<Order> list = orderRepository.findAll();
 		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
 	}
-	
+
 	@Transactional(readOnly = true)
 	public List<OrderDTO> findAllOrdenado() {
 		List<Order> list = orderRepository.findOrdersWithProducts();
 		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
+
+	@SuppressWarnings("deprecation")
+	@Transactional
+	public OrderDTO insert(OrderDTO dto) {
+		Order order = new Order(null, dto.getAddress(), dto.getLongitude(), dto.getLatitude(), Instant.now(),
+				OrderStatus.PENDING);
+		for (ProductDTO p : dto.getList()) {
+			Product product = productRepository.getOne(p.getId());
+			order.getProducts().add(product);
+		}
+		order = orderRepository.save(order);
+		return new OrderDTO(order);
 	}
 }

90623d230943397b4d12fc853a8c0dd150248580
ferrazandre <55859517+ferrazandre@users.noreply.github.com>
2021-08-02 13:07:48 -0300

    create home page

---
+++ front-web/src/Home/index.tsx
@@ -0,0 +1,26 @@
+import './style.css';
+import {ReactComponent as MainImage} from './main.svg';
+
+function Home() {
+    return (
+       <div className="home-container">
+           <div className="home-content">
+                <div className="home-actions">
+                    <h1 className="home-title">Faça o seu pedido <br></br> 
+                        que entregamos <br></br> 
+                        para você!
+                    </h1>
+                    <h3 className="home-subtitle">Escolha seu pedido e em poucos minutos <br></br>
+                     levaremos na sua porta.
+                    </h3>
+                    <a href="#" className="home-btn-order">FAZER PEDIDO</a>
+                </div>
+                <div className="home-image">
+                    <MainImage></MainImage>
+                </div>
+           </div>
+       </div>
+    )
+}
+
+export default Home;
